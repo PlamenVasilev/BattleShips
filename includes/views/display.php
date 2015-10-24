3be93381c5eca730309c3f9830f0bf41be57fd04
Plamen Vasilev <plamen.vasilev.vasilev@gmail.com>
2015-10-24 20:39:21 +0300

    remove short open tags

--- includes/views/display.php
+++ includes/views/display.php
@@ -5,18 +5,18 @@
 </head>
 <body>
 <pre><?=$this->display_grid($show_ships);?></pre>
-<? if($shoot){ ?>
+<?php if($shoot){ ?>
 <p><?=$shoot?></p>
-<? } ?>
-<? if(!$this->game_end){ ?>
+<?php } ?>
+<?php if(!$this->game_end){ ?>
 <form method="post" action="">
 	Help func: show, reset<br />
 	Enter coordinates (row, col), e.g. A5 
 	<input type="text" name="coords" value="" size="5" autocomplete="off" autofocus=""/>
 	<input type="submit" value="Submit" />
 </form>
-<? }else{ ?>
+<?php }else{ ?>
 <a href="">Start new game</a>
-<? } ?>
+<?php } ?>
 </body>
 </html>
